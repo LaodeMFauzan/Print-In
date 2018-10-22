768d30adc7f6955465019b2806318c1bae323cb5
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-10-22 22:22:23 +0700

    Initial commit

---
+++ app/src/main/java/papb/learn/fauzan/printin/LoginActivity.java
@@ -0,0 +1,13 @@
+package papb.learn.fauzan.printin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
