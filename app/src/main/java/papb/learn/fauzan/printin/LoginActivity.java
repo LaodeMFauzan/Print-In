8edb0f4af79ff009ffc24bbb2946001ce59847cf
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-10-27 23:28:42 +0700

    changed : register button in login

--- app/src/main/java/papb/learn/fauzan/printin/LoginActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/LoginActivity.java
@@ -1,13 +1,31 @@
 package papb.learn.fauzan.printin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_register = findViewById(R.id.btn_register);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_register :
+                Intent i = new Intent();
+                break;
+
+        }
     }
 }
