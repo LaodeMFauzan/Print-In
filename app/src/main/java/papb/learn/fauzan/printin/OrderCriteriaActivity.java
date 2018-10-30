4d596dee6995d98c57e82142eee1f9ad44242aaa
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-10-30 23:22:17 +0700

    added : rv criteria order 1/2 complete

--- app/src/main/java/papb/learn/fauzan/printin/OrderCriteriaActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/OrderCriteriaActivity.java
@@ -1,4 +1,31 @@
 package papb.learn.fauzan.printin;
 
-public class OrderCriteriaActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import papb.learn.fauzan.printin.adapter.OrderCriteriaAdapter;
+
+public class OrderCriteriaActivity extends Activity {
+    private RecyclerView  rvOrder;
+    private RecyclerView.Adapter adapterOrderCriteria;
+    private RecyclerView.LayoutManager lmOrderCriteria;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_criteria);
+        rvOrder = findViewById(R.id.rv_order_criteria);
+
+        rvOrder.setHasFixedSize(true);
+
+        lmOrderCriteria = new LinearLayoutManager(this);
+        rvOrder.setLayoutManager(lmOrderCriteria);
+
+        String[] dataSet = new String[10];
+        adapterOrderCriteria = new OrderCriteriaAdapter(dataSet);
+        rvOrder.setAdapter(adapterOrderCriteria);
+    }
 }
---
+++ app/src/main/java/papb/learn/fauzan/printin/adapter/OrderCriteriaAdapter.java
@@ -0,0 +1,27 @@
+package papb.learn.fauzan.printin.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class OrderCriteriaAdapter extends RecyclerView.Adapter {
+
+
+    public OrderCriteriaAdapter(String[] dataSet) {
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
