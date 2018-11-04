841ae82dc326d57d7dc11149317deeeb1bea2a4a
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-11-04 19:04:36 +0700

    added : counting distance and draw line --crash fixed : padding and button next intent

--- app/src/main/java/papb/learn/fauzan/printin/OrderCriteriaActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/OrderCriteriaActivity.java
@@ -1,26 +1,34 @@
 package papb.learn.fauzan.printin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 import papb.learn.fauzan.printin.adapter.OrderCriteriaAdapter;
 import papb.learn.fauzan.printin.model.OrderCriteriaModel;
 
-public class OrderCriteriaActivity extends AppCompatActivity {
+public class OrderCriteriaActivity extends AppCompatActivity implements View.OnClickListener {
     private RecyclerView  rvOrder;
     private OrderCriteriaAdapter adapterOrderCriteria;
     private RecyclerView.LayoutManager lmOrderCriteria;
+
+    private Button btn_save_criteria;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_criteria);
+        btn_save_criteria = findViewById(R.id.btn_simpan_kriteria);
+
+        btn_save_criteria.setOnClickListener(this);
 
         showListOrder();
     }
@@ -39,11 +47,21 @@ public class OrderCriteriaActivity extends AppCompatActivity {
 
     private ArrayList<OrderCriteriaModel> initializeData(){
         ArrayList<OrderCriteriaModel> orderCriteriaModels = new ArrayList<>();
-        OrderCriteriaModel orderTest1 = new OrderCriteriaModel("a","a","a","a","a",true,true);
-        OrderCriteriaModel orderTest2 = new OrderCriteriaModel("a","a","a","a","a",false,true);
+        OrderCriteriaModel orderTest1 = new OrderCriteriaModel("Laporan Sisop","a","a","a","a",true,true);
+        OrderCriteriaModel orderTest2 = new OrderCriteriaModel("Proposal Skripsi","a","a","a","a",false,true);
         orderCriteriaModels.add(orderTest1);
         orderCriteriaModels.add(orderTest2);
 
         return orderCriteriaModels;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_simpan_kriteria :
+                Intent toMapDelivery = new Intent(this,OrderMapsActivity.class);
+                startActivity(toMapDelivery);
+                break;
+        }
     }
 }
--- app/src/main/java/papb/learn/fauzan/printin/OrderMapsActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/OrderMapsActivity.java
@@ -1,13 +1,14 @@
 package papb.learn.fauzan.printin;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -19,11 +20,28 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class OrderMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import papb.learn.fauzan.printin.parser.JSONParserTask;
+
+public class OrderMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private static final int LOCATION_REQUEST = 500;
+    private LatLng delhi = new LatLng(28.6139, 77.2090);
+    private LatLng chandigarh = new LatLng(30.7333, 76.7794);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +71,57 @@ public class OrderMapsActivity extends AppCompatActivity implements OnMapReadyCa
         });
     }
 
+    private class FetchUrl extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+
+            try {
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            ParserTask parserTask = new ParserTask();
+            parserTask.execute(s);
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            iStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
 
     /**
      * Manipulates the map once available.
@@ -66,25 +135,96 @@ public class OrderMapsActivity extends AppCompatActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(chandigarh).title("Chandigarh"));
+        mMap.addMarker(new MarkerOptions().position(delhi).title("Delhi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
 
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST);
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
+        String str_origin = "origin=" + delhi.latitude + "," + delhi.longitude;
+        String str_dest = "destination=" + chandigarh.latitude + "," + chandigarh.longitude;
+        String sensor = "sensor=false";
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + JSONParserTask.MY_API_KEY;
+
+        Log.d("onMapClick", url.toString());
+        FetchUrl FetchUrl = new FetchUrl();
+        FetchUrl.execute(url);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(7));
+
+        Location delhi_location = new Location("Delhi");
+        delhi_location.setLatitude(delhi.latitude);
+        delhi_location.setLongitude(delhi.longitude);
+
+        Location chandigarh_location = new Location("Chandigarh");
+        chandigarh_location.setLatitude(chandigarh.latitude);
+        chandigarh_location.setLongitude(chandigarh.longitude);
+
+        double distance = (delhi_location.distanceTo(chandigarh_location))* 0.000621371 ;
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Info");
+        alertDialog.setMessage("Distance between these two location is : "+distance +" miles");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case LOCATION_REQUEST:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    mMap.setMyLocationEnabled(true);
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+        //TODO (1) : read and understand the code below
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                JSONParserTask parser = new JSONParserTask();
+                Log.d("ParserTask", parser.toString());
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = result.get(i);
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+                    points.add(position);
                 }
-                break;
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.RED);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
         }
     }
 }
