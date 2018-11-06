98df2889ee8ff640364e42483785c5103086fda9
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-11-06 22:30:34 +0700

    added : order summary activity and layout

---
+++ app/src/main/java/papb/learn/fauzan/printin/OrderSummaryActivity.java
@@ -0,0 +1,14 @@
+package papb.learn.fauzan.printin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrderSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_summary);
+    }
+}
