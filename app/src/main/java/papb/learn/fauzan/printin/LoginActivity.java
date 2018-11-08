4073ea544fe773a821350aa1a107839e7330350c
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-11-08 22:22:15 +0700

    added : firebase email authentication

--- app/src/main/java/papb/learn/fauzan/printin/LoginActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/LoginActivity.java
@@ -1,19 +1,35 @@
 package papb.learn.fauzan.printin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_register;
     private Button btn_login;
+    private EditText et_email_login,et_password_login;
     int reqCode = 99;
 
+    private FirebaseAuth mAuth;
+
     private String name,email,password;
+
+    final String TAG_LOGIN_SUCCESS = "SUCCESS";
+    final String TAG_LOGIN_FAILED= "ERROR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +38,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         tv_register = findViewById(R.id.tv_register);
         btn_login = findViewById(R.id.btn_login);
+        et_email_login = findViewById(R.id.et_email_login);
+        et_password_login = findViewById(R.id.et_password_login);
 
         tv_register.setOnClickListener(this);
         btn_login.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null){
+            Intent toHomeActivity = new Intent(this,OrderCriteriaActivity.class);
+            startActivity(toHomeActivity);
+        }
     }
 
     @Override
@@ -36,8 +70,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
 
             case R.id.btn_login:
-                Intent toOrderCriteriaTemp = new Intent(this,OrderCriteriaActivity.class);
-                startActivity(toOrderCriteriaTemp);
+//                Intent toOrderCriteriaTemp = new Intent(this,OrderCriteriaActivity.class);
+//                startActivity(toOrderCriteriaTemp);
+                email = et_email_login.getText().toString();
+                password = et_password_login.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Log.d(TAG_LOGIN_SUCCESS,"createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    Log.w(TAG_LOGIN_FAILED,"createUserWithEmail:failure",task.getException());
+                                    Toast.makeText(LoginActivity.this,"Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
                 break;
 
         }
--- app/src/main/java/papb/learn/fauzan/printin/RegisterActivity.java
+++ app/src/main/java/papb/learn/fauzan/printin/RegisterActivity.java
@@ -2,16 +2,28 @@ package papb.learn.fauzan.printin;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText et_username,et_email,et_password;
     private Button btn_register;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +35,58 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         et_password = findViewById(R.id.et_password_register);
 
         btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseAuth.getInstance().signOut();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null){
+            Intent toLoginActivity = getIntent();
+            setResult(RESULT_OK,toLoginActivity);
+            finish();
+        }
+    }
+
+    private void createUser(){
+        String email_register = et_email.getText().toString();
+        String password_register = et_password.getText().toString();
+
+        final String TAG_SUCCESS_CREATE = "SUCCESS";
+        final String TAG_FAILED_CREATE = "ERROR";
+
+        mAuth.createUserWithEmailAndPassword(email_register,password_register)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG_SUCCESS_CREATE,"createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG_FAILED_CREATE,"createUserWithEmail:failure",task.getException());
+                            Toast.makeText(RegisterActivity.this,"Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_register:
-                Intent backToLogin = getIntent();
-                backToLogin.putExtra("email",et_email.getText().toString());
-                backToLogin.putExtra("username",et_username.getText().toString());
-                backToLogin.putExtra("password",et_password.getText().toString());
-
-                setResult(RESULT_OK,backToLogin);
-                finish();
+                createUser();
         }
     }
 }
